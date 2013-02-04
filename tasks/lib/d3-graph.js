9e7c5cffd3fa93d0e41045b08c3e81753378ec65
Kenneth Auchenberg <kenneth@auchenberg.dk>
2013-02-04 23:16:29 +0100

    New rounded lines, and arrows between nodes.

--- tasks/lib/d3-graph.js
+++ tasks/lib/d3-graph.js
@@ -21,6 +21,10 @@
   }
 
   function fadeRelatedNodes(d, opacity, nodes, links) {
+
+    // Clean
+    $('path.link').removeAttr('data-show');
+
     nodes.style("stroke-opacity", function(o) {
 
       if (isConnected(d, o)) {
@@ -30,24 +34,44 @@
       }
 
       this.setAttribute('fill-opacity', thisOpacity);
+      this.setAttribute('stroke-opacity', thisOpacity);
+
+      if(thisOpacity == 1) {
+        this.classList.remove('dimmed');
+      } else {
+        this.classList.add('dimmed');
+      }
 
       return thisOpacity;
     });
 
     links.style("stroke-opacity", function(o) {
 
-      if (o.source === d || o.target === d) {
+      if (o.source === d) {
 
         // Highlight target/sources of the link
-        var selector = '.'+formatClassName('node', o.source) + ', .' + formatClassName('node', o.target);
-        var nodes = graph.selectAll(selector);
+        var elmNodes = graph.selectAll('.'+formatClassName('node', o.target));
+        elmNodes.attr('fill-opacity', 1);
+        elmNodes.attr('stroke-opacity', 1);
 
-        nodes.attr('fill-opacity', 1);
-        nodes.attr('stroke-opacity', 1);
+        elmNodes.classed('dimmed', false);
+
+        // Highlight arrows
+        var elmCurrentLink = $('path.link[data-source=' + o.source.index + ']');
+        elmCurrentLink.attr('data-show', true);
+        elmCurrentLink.attr('marker-end', 'url(#regular)');
 
         return 1;
+
       } else {
 
+        var elmAllLinks = $('path.link:not([data-show])');
+
+        if(opacity == 1) {
+          elmAllLinks.attr('marker-end', 'url(#regular)');
+        } else {
+          elmAllLinks.attr('marker-end', '');
+        }
 
         return opacity;
       }
@@ -61,25 +85,20 @@
     zoom = d3.behavior.zoom();
     zoom.on("zoom", onZoomChanged);
 
-
     // Setup layout
     layout = d3.layout.force()
       .gravity(.05)
       .charge(-300)
-      .linkDistance(200);
-
+      .linkDistance(100);
 
     // Setup graph
     graph = d3.select(".graph")
       .append("svg:svg")
       .attr("pointer-events", "all")
       .call(zoom)
-      .append('svg:g');
-
-    graph.append('svg:rect')
-      .attr('width', graphWidth)
-      .attr('height', graphHeight)
-      .attr('fill', 'rgba(1,1,1,0)')
+      .append('svg:g')
+        .attr('width', graphWidth)
+        .attr('height', graphHeight);
 
     d3.select(window).on("resize", resize);
 
@@ -87,8 +106,12 @@
     var graphData = window.getGraphData();
     renderGraph(graphData);
 
+    data = graphData;
+
     // Resize
     resize();
+
+    centerGraph();
 
     // Controlers
     $('.control-zoom a').on('click', onControlZoomClicked);
@@ -104,20 +127,49 @@
           .resume();
   }
 
+  function centerGraph() {
+
+    var centerTranslate = [
+      (graphWidth / 2) - (graphWidth * 0.2 / 2),
+      (graphHeight / 2) - (graphHeight * 0.2 / 2),
+    ];
+
+    zoom.translate(centerTranslate);
+
+    // Render transition
+    graph.transition()
+      .duration(500)
+      .attr("transform", "translate(" + zoom.translate() + ")" + " scale(" + zoom.scale() + ")");
+  }
 
   function renderGraph(data) {
 
-    // Lines
-    links = graph.selectAll("line")
-      .data(data.links)
-      .enter().append("line")
-      .attr("stroke-width","1.5")
-      .style("stroke", '#ccc');
 
-      links.append("title").text(function(d) { return d.label });
+    // Markers
+    graph.append("svg:defs").selectAll("marker")
+        .data(['regular'])
+      .enter().append("svg:marker")
+        .attr("id", String)
+        .attr("viewBox", "0 -5 10 10")
+        .attr("refX", 15)
+        .attr("refY", -1.5)
+        .attr("markerWidth", 6)
+        .attr("markerHeight", 6)
+        .attr("orient", "auto")
+      .append("svg:path")
+        .attr("d", "M0,-5L10,0L0,5");
+
+    // Lines
+    links = graph.append('svg:g').selectAll("line")
+      .data(data.links)
+      .enter().append("svg:path")
+      .attr('class', 'link')
+      .attr("data-target", function(o) { return o.target })
+      .attr("data-source", function(o) { return o.source })
+      .attr("marker-end", function(d) { return "url(#regular)"; });
 
     // Nodes
-    nodes = graph.selectAll("g.node")
+    nodes = graph.append('svg:g').selectAll("node")
       .data(data.nodes)
       .enter().append("svg:g")
       .attr("class","node")
@@ -128,25 +180,28 @@
       nodes.attr("class", function(d) { return formatClassName('node', d) })
       nodes.append("svg:circle")
         .attr("class", function(d) { return formatClassName('circle', d) })
-        .attr("r", 10)
+        .attr("r", 6)
         .on("mouseover", _.bind(onNodeMouseOver, this, nodes, links))
         .on("mouseout", _.bind(onNodeMouseOut, this, nodes, links) );
 
-      // Label
+
+      // A copy of the text with a thick white stroke for legibility.
       nodes.append("svg:text")
-        .attr("class", function(d) { return formatClassName('text', d) })
-        .attr("text-anchor", "middle")
-        .attr("fill","#000")
-        .style("pointer-events", "none")
-        .attr("font-size", '10px')
-        .attr("dy", function() { return -20; })
-        .text( function(d) { return d.id; });
+          .attr("x", 15)
+          .attr("y", ".31em")
+          .attr("class", function(d) { return 'shadow ' + formatClassName('text', d) })
+          .text(function(d) { return d.id; });
+
+      nodes.append("svg:text")
+          .attr("class", function(d) { return formatClassName('text', d) })
+          .attr("x", 15)
+          .attr("y", ".31em")
+          .text(function(d) { return d.id; });
 
     // Build linked index
     data.links.forEach(function(d) {
       linkedByIndex[d.source.index + "," + d.target.index] = 1;
     });
-
 
     // Draw the
     layout.nodes(data.nodes);
@@ -154,21 +209,12 @@
     layout.on("tick", onTick);
     layout.start();
 
-
-
-    var centerTranslate = [
-      (graphWidth / 2) - (graphWidth * 0.2 / 2),
-      (graphHeight / 2) - (graphHeight * 0.2 / 2),
-    ];
-
-    zoom
-      .translate(centerTranslate)
-      .scale(0.2);
+    zoom.scale(0.4);
 
     // Render transition
     graph.transition()
       .duration(500)
-      .attr("transform", "translate(" + zoom.translate() + ")" + " scale(" + zoom.scale() + ")");
+      .attr("transform", "scale(" + zoom.scale() + ")");
 
   }
 
@@ -194,26 +240,20 @@
 
   function onTick(e) {
 
-    // // Push sources up and targets down to form a weak tree.
-    // var k = 6 * e.alpha;
-    // data.links.forEach(function(d, i) {
-    //   d.source.y -= k;
-    //   d.target.y -= k;
-    // });
+    links.attr("d", function(d) {
+      var dx = d.target.x - d.source.x,
+          dy = d.target.y - d.source.y,
+          dr = Math.sqrt(dx * dx + dy * dy);
+      return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+    });
 
     nodes.attr("cx", function(d) { return d.x; })
          .attr("cy", function(d) { return d.y; })
          .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-
-    links.attr("x1", function(d) { return d.source.x; })
-         .attr("y1", function(d) { return d.source.y; })
-         .attr("x2", function(d) { return d.target.x; })
-         .attr("y2", function(d) { return d.target.y; });
   }
 
   function onControlZoomClicked(e) {
     var elmTarget = $(this)
-
     var scaleProcentile = 0.20;
 
     // Scale
